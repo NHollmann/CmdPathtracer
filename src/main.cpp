d4c8156168f6c21da5808bdb7b390b399d658c54
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-07-23 03:24:46 +0200

    Complete chapter 12

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,44 @@ inline int colorFloatToInt(floating color)
     return (int)(255.99f * color);
 }
 
+scene::Hitable *randomScene()
+{
+    const int N = 500;
+    scene::Hitable **list = new scene::Hitable*[N+1];
+    list[0] = new scene::Sphere(math::Vector3(0, -1000, 0), 1000, new mat::Lambertian(math::Vector3(0.5, 0.5, 0.5)));
+    int i = 1;
+
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            floating chooseMat = drand48();
+            math::Vector3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - math::Vector3(4, 0.2, 0)).length() > 0.9)
+            {
+                if (chooseMat < 0.8)
+                {
+                    list[i++] = new scene::Sphere(center, 0.2, new mat::Lambertian(math::Vector3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                }
+                else if (chooseMat < 0.95)
+                {
+                    list[i++] = new scene::Sphere(center, 0.2, new mat::Metal(math::Vector3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48()));
+                }
+                else
+                {
+                    list[i++] = new scene::Sphere(center, 0.2, new mat::Dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new scene::Sphere(math::Vector3(0, 1, 0), 1.0, new mat::Dielectric(1.5));
+    list[i++] = new scene::Sphere(math::Vector3(-4, 1, 0), 1.0, new mat::Lambertian(math::Vector3(0.4, 0.2, 0.1)));
+    list[i++] = new scene::Sphere(math::Vector3(4, 1, 0), 1.0, new mat::Metal(math::Vector3(0.7, 0.6, 0.5), 0.0));
+
+    return new scene::World(list, i);
+}
+
 int main()
 {
     const int width = 200;
@@ -55,18 +93,19 @@ int main()
         return 1;
     }
 
-    scene::Hitable *list[5];
+    /* scene::Hitable *list[5];
     list[0] = new scene::Sphere(math::Vector3(0, 0, -1), 0.5, new mat::Lambertian(math::Vector3(0.1, 0.2, 0.5)));
     list[1] = new scene::Sphere(math::Vector3(0, -100.5, -1), 100, new mat::Lambertian(math::Vector3(0.8, 0.8, 0.0)));
     list[2] = new scene::Sphere(math::Vector3(1, 0, -1), 0.5, new mat::Metal(math::Vector3(0.8, 0.6, 0.2), 0.2));
     list[3] = new scene::Sphere(math::Vector3(-1, 0, -1), 0.5, new mat::Dielectric(1.5));
     list[4] = new scene::Sphere(math::Vector3(-1, 0, -1), -0.45, new mat::Dielectric(1.5));
-    scene::Hitable *world = new scene::World(list, 5);
+    scene::Hitable *world = new scene::World(list, 5); */
+    scene::Hitable *world = randomScene();
 
-    math::Vector3 lookFrom(3, 3, 2);
-    math::Vector3 lookAt(0, 0, -1);
-    floating distToFocus = (lookFrom - lookAt).length();
-    floating aperture = 2.0;
+    math::Vector3 lookFrom(13, 2, 3);
+    math::Vector3 lookAt(0, 0, 0);
+    floating distToFocus = 10.0;
+    floating aperture = 0.1;
 
     Camera camera(
         lookFrom,
