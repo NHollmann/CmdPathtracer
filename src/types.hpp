29bd415d21c88b4766892e70b9c7de795dbb116c
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-07-27 17:04:26 +0200

    Windows compatibility

--- src/types.hpp
+++ src/types.hpp
@@ -8,4 +8,15 @@ typedef double floating;
 typedef float floating;
 #endif
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #define MAX_FLOATING (std::numeric_limits<floating>::max())
+
+#ifdef WIN32
+inline double drand48()
+{
+    return (rand() / (RAND_MAX + 1.0));
+}
+#endif
